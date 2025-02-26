a23f57ffc3f53e91116f5bd6bd12a17dc451ac6c
Valdaren <bdog0804@gmail.com>
2025-02-26 17:35:30 -0600

    Delete third-page directory

--- third-page/script.js
+++
@@ -1,8 +0,0 @@
-function openHeart() {
-    const container = document.querySelector('.container');
-    container.classList.add('open');
-    container.removeEventListener('click', openHeart);
-}
-
-// Adding the event listener to the container
-document.querySelector('.container').addEventListener('click', openHeart);
